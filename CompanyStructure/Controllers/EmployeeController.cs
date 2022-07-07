aa0825220b603c373124c178d6283c109119c936
Klaudia Budzynska <klaudia.budzynska1@gmail.com>
2022-07-07 20:23:18 +0200

    Database setup

---
+++ CompanyStructure/Controllers/EmployeeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyStructure.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController
+    {
+    }
+}
---
+++ CompanyStructure/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyStructure.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<EmployeeItem> EmployeeItems { get; set; } = null!;
+    }
+}
